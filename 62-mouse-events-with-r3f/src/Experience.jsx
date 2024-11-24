9cef1c619360e8d98a400a17c488340c7978b263
Alexander Nuzhnyi <hello@alexander.works>
2024-11-24 23:07:16 +0100

    Apply a random color by clicking an object

--- 62-mouse-events-with-r3f/src/Experience.jsx
+++ 62-mouse-events-with-r3f/src/Experience.jsx
@@ -1,37 +1,54 @@
-import { useFrame } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
-import { useRef } from 'react'
+import { useFrame } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+import { useRef } from 'react';
 
-export default function Experience()
-{
-    const cube = useRef()
-    
-    useFrame((state, delta) =>
-    {
-        cube.current.rotation.y += delta * 0.2
-    })
+export default function Experience() {
+  const cube = useRef();
 
-    return <>
+  useFrame((state, delta) => {
+    cube.current.rotation.y += delta * 0.2;
+  });
 
-        <OrbitControls makeDefault />
+  const colorsArr = [
+    'orange',
+    'purple',
+    'mediumpurple',
+    'green',
+    'blue',
+    'red',
+  ];
 
-        <directionalLight position={ [ 1, 2, 3 ] } intensity={ 4.5 } />
-        <ambientLight intensity={ 1.5 } />
+  const getRandomColor = () => {
+    return colorsArr[Math.floor(Math.random() * colorsArr.length)];
+  };
 
-        <mesh position-x={ - 2 }>
-            <sphereGeometry />
-            <meshStandardMaterial color="orange" />
-        </mesh>
+  const eventHandler = (e) => {
+    console.log('The event occured!');
+    e.object.material.color.set(getRandomColor());
+    // console.log(e.object.material);
+  };
 
-        <mesh ref={ cube } position-x={ 2 } scale={ 1.5 }>
-            <boxGeometry />
-            <meshStandardMaterial color="mediumpurple" />
-        </mesh>
+  return (
+    <>
+      <OrbitControls makeDefault />
 
-        <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
-            <planeGeometry />
-            <meshStandardMaterial color="greenyellow" />
-        </mesh>
+      <directionalLight position={[1, 2, 3]} intensity={4.5} />
+      <ambientLight intensity={1.5} />
 
+      <mesh position-x={-2} onClick={eventHandler}>
+        <sphereGeometry />
+        <meshStandardMaterial color="orange" />
+      </mesh>
+
+      <mesh ref={cube} position-x={2} scale={1.5} onClick={eventHandler}>
+        <boxGeometry />
+        <meshStandardMaterial color="mediumpurple" />
+      </mesh>
+
+      <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
+        <planeGeometry />
+        <meshStandardMaterial color="greenyellow" />
+      </mesh>
     </>
+  );
 }
